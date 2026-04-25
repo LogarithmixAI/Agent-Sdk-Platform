75089099c255be015b7b25fdaa4bad83ce745aac
SHUBHAM GARG <101034519+ShubhamCoder-In@users.noreply.github.com>
2026-04-26 03:57:13 +0530

    Add CI pipeline

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from app import create_app
 from app.config import config
 import os
 
-env = os.getenv('FLASK_ENV', 'production')
+env = os.getenv('FLASK_ENV', 'deveploment')
 app = create_app(config[env])
 
 if __name__ == '__main__':
